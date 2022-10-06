d49364b546f89a13c8b0d10ccf8b54c9f2e58137
hh-Nghiep <nghiephh2000@gmail.com>
2022-10-06 23:42:54 +0700

    push 1433

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -9,7 +9,7 @@ const config = {
         enableArithAbort: true,
         instancename: 'SQLEXPRESS'
     },
-    port: 65534
+    port: 1433
 }
 
-module.exports = config; 
+module.exports = config;
